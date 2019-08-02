ed1e21804614686b58b8269019eea973876db1ea
Amberly Brewster <amberlybrewster@Amberlys-iMac.local>
2019-08-02 13:41:43 -0400

    Working

---
+++ Hello Unity World Redo/Assets/ChangeName.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeName : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            TextMeshProUGUI textmeshPro = GetComponent<TextMeshProUGUI>();
+            textmeshPro.SetText("Hello World from Amberly Brewster");
+        }
+    }
+}
